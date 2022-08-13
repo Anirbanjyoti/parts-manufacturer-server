0972012c62594900c2c312a641d85d8459b140bb
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-08-13 10:42:43 +0530

    UPsert admin field to user Collection

--- index.js
+++ index.js
@@ -106,8 +106,20 @@ async function run() {
       );
       res.send({ result, token });
     });
+        // UPsert admin field to user Collection ANd denied unauthorized access by verifyJWT
+        app.put("/user/admin/:email", verifyJWT, verifyAdmin, async (req, res) => {
+          const email = req.params.email;
+          const filter = { email: email };
+          const updateDoc = {
+            $set: { role: "admin" },
+          };
+          const result = await userCollection.updateOne(filter, updateDoc);
+          res.send(result);
+        });
 
-    
+
+
+
   } finally {
     //
   }
